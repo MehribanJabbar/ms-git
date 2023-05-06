75b4b9c14f5b724cd40c1d685fc151395707da96
MehribanJabbar <memka.cabbarli@gmail.com>
2023-05-06 11:53:01 +0400

    tmp1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world.");
         System.out.println("Test");
         System.out.println();
-        System.out.println("Hello Ingress");
+        System.out.println("Hello Ingresss");
     }
 }
