1b32163a414bca7be9e624227238b72ed4135f55
MehribanJabbar <memka.cabbarli@gmail.com>
2023-05-06 11:29:21 +0400

    Added new line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world.");
         System.out.println("Test");
         System.out.println();
+        System.out.println("Hello INGRESS");
     }
 }
